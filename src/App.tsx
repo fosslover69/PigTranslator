7d60db05257dd8527d093dc854c43efecd8c6d78
Ravishanker V.G <no.1opensourcelover@protonmail.com>
2022-12-07 16:13:38 +0530

    PigTranslator: Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [pigLatin, setPigLatin] = useState("");
+  return (
+    <div className="App">
+      <div className="main">
+        <div className="title">Pig Translator</div>
+        <textarea
+          className="input"
+          placeholder="Enter something in English"
+          onChange={(e) => setPigLatin(pigLogic(e.target.value))}
+        />
+      </div>
+      <div className="output">
+        <div className="text">Your text in Pig Latin:</div>
+        <div className="result">{pigLatin}</div>
+      </div>
+    </div>
+  );
+}
+function pigLogic(value: string) {
+  const tempPig = value.split("\n");
+  let pigValue: string[] = [];
+  tempPig.map((value) => {
+    let splitedValue = value.split(" ");
+    splitedValue.map((value) => {
+      pigValue.push(value);
+    });
+  });
+  const vowels = ["a", "e", "i", "o", "u"];
+  let finalValue = "";
+  console.log(pigValue);
+  pigValue.map((value) => {
+    if (value != "") {
+      if (vowels.includes(value.charAt(0))) {
+        if (value.endsWith("\n")) {
+          value = value.slice(0, -1);
+          value += "yay";
+          finalValue += value + "\n";
+        } else {
+          value += "yay";
+          finalValue += value + " ";
+        }
+      } else {
+        if (value.endsWith("\n")) {
+          value = value.slice(0, -1);
+          value += value.charAt(0) + "ay";
+          value = value.slice(1);
+          finalValue += value + "\n";
+        } else {
+          value += value.charAt(0) + "ay";
+          value = value.slice(1);
+          finalValue += value + " ";
+        }
+      }
+    }
+  });
+  return finalValue;
+}
+
+export default App;
